794a3c7d499e7bb2b9c3ad3321ddc22d394bd7df
bigblackwolf <bigblackwolf@bigblackwolfs-MacBook-Pro.local>
2023-09-06 17:17:34 +0200

    Initial

---
+++ README.md
@@ -0,0 +1,22 @@
+# Dead Code Detector
+
+A tool designed to identify unused code.
+
+# Quick start
+
+Clone repository
+```shell script
+git clone https://github.com/BigBlackWolf/deadcode-detector.git
+cd deadcode-detector
+```
+
+Create virtual environment and install dependancies
+```shell script
+python -m virtualenv venv
+source venv/bin/activate
+```
+
+Run the command against needed directory
+```shell script
+python src/main.py
+```
---
+++ tests/py_tests.py
@@ -0,0 +1,37 @@
+# Definition
+class Executable:
+    pass
+
+# Positive
+class_ AnotherClass(Executable):
+test_class = Executable
+class AnotherClass(Executable):
+c.Executable()
+bsds():v.Executable
+bsds():v.Executable(asd, asd)
+Executable,
+    Executable,
+    Executable(),
+     asd().Executable(),
+def Executable2(Executable,
+def   Executable_2(Executable)
+
+# Negative
+class Executable(Executable
+classExecutable: str
+class_Executable()
+classname._Executable()
+Executable1()
+class      Executable:
+class      Executable(:
+    class Executabe(
+     Executable2(),
+     ExecutableF.dg
+asd vExecutable() ->:
+asd Executable_v():
+class Executable-1(
+asd Executable-test():
+Executable-test():
+asd Executable8v():
+def  Executable()
+    def  Executable()
